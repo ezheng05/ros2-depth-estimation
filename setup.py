605d843b8821458209426feb43d92c03c459a68f
ezpro23 <23ezheng@unis.org>
2026-04-30 00:01:49 -0400

    fix setup

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ setup(
     data_files=[
         ('share/ament_index/resource_index/packages', ['resource/' + package_name]),
         ('share/' + package_name, ['package.xml']),
-        ('share/' + package_name + '/launch', ['launch/depth_launch.py', 'launch/rviz_launch.py']),
+        ('share/' + package_name + '/launch', ['launch/depth_launch.py']),
     ],
     install_requires=['setuptools'],
     zip_safe=True,
